7644bdcec689ca9df5d6d01734ada375abf8488d
Alekhya1908 <61342303+Alekhya1908@users.noreply.github.com>
2020-06-22 23:16:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,44 @@
 import React from 'react';
 import './App.css';
-import data from './data/data.json'
-import {BrowserRouter,Route,Link} from 'react-router-dom';
-import Reac_resume from './Reac_resume';
-import logo from './profile_pic.svg';
+import profilepic from './delivery-man.svg'
+import data from './data.json';
 import './css/bootstrap.min.css';
-
-
+import {BrowserRouter,Route,Link} from 'react-router-dom';
+import Resume from'./resume';
 function App() {
   return (
-    <BrowserRouter >
-    <Route exact path="/proreact" component={Home}></Route>
-    <Route exact path="/resume" component={Reac_resume}></Route>
-    </BrowserRouter>
-  );
-}
-
-let Home=()=>
- {
-   let  profiles=data.profile;
+    <BrowserRouter>
+    <Route exact path='/' component={Home}/>
+    <Route exact path='/resume' component={Resume}/>
+      </BrowserRouter>
+   );
+ };
+ let Home=()=>{
+   let profiles=data.profile;
    console.log(profiles);
-   return( 
-    <div>
-    <div className="row justify-content-center">
-        {profiles.map((values,index)=>(
-        <div className="col-lg-4 col-md-4 col-sm-10 mt-2" key={index}>
-           <div className="card bg-secondary">
-              <div className="card-body text-center " >
-                <img src={logo} style={{width:"30%"}}></img>
-                <h3>{values.basics.name}</h3>
-                <hr/>
-               
-                <a href={"tel:"+ values.basics.Mobile}>
-                <p className="text-dark">{values.basics.Mobile}</p></a>
-                <a href={"mailto:"+ values.basics.email}>
-                <p className="text-dark">{values.basics.email}</p></a>
-                <p className="">{values.basics.pin}</p>
-
-                <Link to={{ pathname:"/resume",data:{id:index}}} className="btn btn-dark text-white">View Profile</Link>
-              </div>
-            </div>
+   return(
+     <div className="row justify-content-center">
+       {profiles.map((values,index)=>(
+         <div className="col-lg-4 col-md-4 col-sm-10 mt-1" key={index}>
+      <div className="card ">
+          <div  className="card-body text-center">
+           <img src={profilepic} alt="pic" style={{width:"30%"}}/>
+            <h2>{values.basics.name}</h2>
+            <h4 className="text-secondary">{values.basics.role}</h4>
+         <a href={"tel:"+values.basics.phone}>   
+          <h4>{values.basics.phone}</h4>
+              </a> 
+         <a href={"mailto:"+values.basics.email}>
+             <h4>{values.basics.email}</h4>
+         </a>
+         <Link to={{pathname:'/resume',data:{id:index}}} className="btn btn-primary">View Profile</Link>
+           </div>
          </div>
-    ))}
-    </div>
-</div>
+         </div>
+       ))}
+     </div>
    )
+ 
  }
 
 export default App;
