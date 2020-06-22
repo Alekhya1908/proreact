b1c074242edadcc616049d7155506ba91d9e178a
Alekhya Mandala <alekhya.mandala99@gmail.com>
2020-06-22 20:31:11 +0530

    Updates

---
+++ precache-manifest.5bf438d97d1d56d634954ec3aa7c0926.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f81b0d19d403229cf50a3ce884e65d9e",
+    "url": "/proreact/index.html"
+  },
+  {
+    "revision": "9ed40f6d391341465061",
+    "url": "/proreact/static/css/main.ab003eb6.chunk.css"
+  },
+  {
+    "revision": "2521124e85243936d40d",
+    "url": "/proreact/static/js/2.37c1c238.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/proreact/static/js/2.37c1c238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ed40f6d391341465061",
+    "url": "/proreact/static/js/main.df8e496b.chunk.js"
+  },
+  {
+    "revision": "9f416c21acb97a631d10",
+    "url": "/proreact/static/js/runtime-main.b8b13245.js"
+  },
+  {
+    "revision": "6f88fc6b60d4b7515f1b0d14efd320a0",
+    "url": "/proreact/static/media/profile_pic.6f88fc6b.svg"
+  }
+]);
